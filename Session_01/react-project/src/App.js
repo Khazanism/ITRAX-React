95e02d8f1eb9e2d28f6eb17de1b683de7a1b8823
Khazanism <khazanyassin@gmail.com>
2022-08-15 21:03:57 +0200

    end of session 01

--- Session_01/react-project/src/App.js
+++ Session_01/react-project/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
 
 function App() {
+  const myName = "ahmed";
+
+  function greeting(name: any) {
+    return <h2> Hello, {name}</h2>;
+  }
+  // const element = <h2> hello, {myName} </h2>;
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> hello react app </h1>
+      {greeting(myName)} 
+      {/* {element} */}
     </div>
+    </>
   );
 }
 
--- Session_01/react-project/src/index.js
+++ Session_01/react-project/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  //   {/* <App /> */}
-  // </React.StrictMode>
-  <h1>HELLO WORLD</h1>
+  <React.StrictMode>
+    {<App />}
+  </React.StrictMode>
+  // <h1>HELLO WORLD</h1>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
