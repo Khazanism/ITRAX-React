81c8823027b14209b4be1d574b677d548f459001
Khazanism <khazanyassin@gmail.com>
2022-08-18 12:27:23 +0200

    session 2

--- session_02/src/App.js
+++ session_02/src/App.js
@@ -1,17 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
-// import Welcome from "./Components/Welcome";
-import AddTask from './components/AddTask';
-import TaskList from './components/TaskList';
+import Welcome from "./Components/Welcome";
+
 function App() {
   return (
-    <>
-    <div className="App">
-      {/* <Welcome /> */}
-      <AddTask />
-      <TaskList />
-    </div>
-    </>
+    
+      <div className="App">
+        <Welcome />
+      </div>
+    
   );
 }
 
